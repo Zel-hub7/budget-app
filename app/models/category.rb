db6fd2e9a717cd04b2761954ed8824f97ff45388
unknown <zelalemyohannes75@gmail.com>
2023-11-27 16:31:42 +0300

    add relation between files

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    has_many :payments, dependent: :destroy
+    has_many :items, through: :payments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    has_many :categories, dependent: :destroy
+    has_many :items, dependent: :destroy
     def index
+
     end
 end
