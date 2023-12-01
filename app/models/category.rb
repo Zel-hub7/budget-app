e942b25b587789f570070e9201ea9e6f536f6b0b
unknown <zelalemyohannes75@gmail.com>
2023-12-01 21:54:30 +0300

    style the login and signup page

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
     belongs_to :user
     has_many :payments, dependent: :destroy
     has_many :items, through: :payments
+
+    def total_price
+        items.sum(:amount)
+    end
 end
