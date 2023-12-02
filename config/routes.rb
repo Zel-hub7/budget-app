593277ac56d77e8aa2a457e6b015904c07f39e99
unknown <zelalemyohannes75@gmail.com>
2023-12-02 19:27:02 +0300

    add splash test

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   end
 
   unauthenticated do
-    root to: 'splash#index'
+    root to: 'splash#index', as: :unauthenticated_root
   end
 
   resources :categories do
---
+++ spec/features/splash_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'splash page', type: :feature do
+  context 'when users is not logged in to the system' do
+    it 'displays the name of the app' do
+      visit unauthenticated_root_path
+
+      expect(page).to have_content('Budget Tracker')
+    end
+  end
+ends
