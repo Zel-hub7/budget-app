59c77a654b1447a50217dda7197cb6e795125edf
unknown <zelalemyohannes75@gmail.com>
2023-12-02 19:02:11 +0300

    add test codes

---
+++ spec/models/payment_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Payment, type: :model do
+  describe 'payment relations' do
+    it 'should belong to item class' do
+      expect(Payment.reflect_on_association(:item).macro).to eq(:belongs_to)
+    end
+
+    it 'should belong to category class' do
+      expect(Payment.reflect_on_association(:category).macro).to eq(:belongs_to)
+    end
+  end
+end
